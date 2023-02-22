2716aefadc54ae783e15ee8895a70caa8ab99304
velguru14 <velguru14@gmail.com>
2023-02-22 08:36:34 +0530

    Customer page add

---
+++ src/main/java/PageObject/CustomerPage.java
@@ -0,0 +1,30 @@
+package PageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CustomerPage {
+	
+	@FindBy(xpath="//button[contains(text(),'Add')]")
+	public WebElement btn_add;
+	
+	@FindBy(name="fname")
+	public WebElement txt_firstName;
+	
+	@FindBy(name="lname")
+	public WebElement txt_lastName;
+	
+	@FindBy(name="email")
+	public WebElement txt_email;
+	
+	@FindBy(name="password")
+	public WebElement txt_password;
+	
+	public  CustomerPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+
+}
